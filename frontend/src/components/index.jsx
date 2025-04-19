a4241d432bc40a23cdf51530dec640ec580e1760
izzuddin.i <izzue5057@gmail.com>
2025-04-19 15:29:34 +0800

    chore: comment unused code

--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -1,4 +1,4 @@
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator } from "./Breadcrumb";
+// import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator } from "./Breadcrumb";
 import { Button } from "./Button";
 import { FloatingLabelInput } from "./FloatingLabelInput";
 import { Heading } from "./Heading";
@@ -10,10 +10,10 @@ import { SelectBox } from "./SelectBox";
 import { Switch } from "./Switch";
 import { Text } from "./Text";
 export {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbSeparator,
+    // Breadcrumb,
+    // BreadcrumbItem,
+    // BreadcrumbLink,
+    // BreadcrumbSeparator,
     Button,
     FloatingLabelInput,
     Heading,
